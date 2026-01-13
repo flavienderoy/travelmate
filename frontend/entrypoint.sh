199055d9b4b24853e5b624cb0f0f4cd91ca4d908
flavienderoy <flav.deroy@outlook.fr>
2026-01-13 16:13:20 +0100

    Fix: Nginx config

--- frontend/entrypoint.sh
+++ frontend/entrypoint.sh
@@ -6,17 +6,16 @@ if [ -z "$PORT" ]; then
     export PORT=80
 fi
 
-echo "Detailed setup using entrypoint..."
-echo "Starting Nginx on port: $PORT"
+echo "Starting Nginx Setup..."
+echo "Target Port: $PORT"
 
-# Replace ${PORT} in the template with the actual value
-# We only substitute ${PORT} to protect other variables like $uri
-envsubst '${PORT}' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
+# Utiliser SED au lieu de envsubst pour être sûr de ne pas casser les variables Nginx ($uri)
+# Remplacement de ${PORT} par la valeur réelle
+sed "s/\${PORT}/$PORT/g" /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
 
-# START DEBUG: View generated config
-echo "--- Generated Nginx Config ---"
+echo "--- VALIDATION CONFIG ---"
 cat /etc/nginx/conf.d/default.conf
-echo "--- End Config ---"
-# END DEBUG
+echo "-------------------------"
 
+# Démarrer Nginx
 exec "$@"
